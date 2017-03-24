829744bc37341c5841983ae8bd7cc57e5736c45c
Leo <leoshch@gmail.com>
2017-03-24 06:20:20 +0000

    update test spdk

--- install_on_aws.sh
+++ install_on_aws.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-##Update kernel and spdk need package, after update kernel ,should reboot VM
-sudo yum install -y gcc gcc-c++ CUnit-devel libaio-devel openssl-devel git nvme-cli
+##Update kernel , after update kernel ,should reboot VM
 sudo yum install -y kernel kernel-devel
-sudo yum upgrade -y
+sleep 10
 sudo reboot
+
+##Install spdk need package and upgrade OS
+sudo yum install -y gcc gcc-c++ CUnit-devel libaio-devel openssl-devel git nvme-cli
+sleep 20
+sudo yum upgrade -y
 
 ##Download SPDK and DPDK
 git clone https://github.com/spdk/spdk
@@ -28,5 +32,21 @@ make DPDK_DIR=./dpdk-17.02/x86_64-native-linuxapp-gcc CONFIG_FIO_PLUGIN=y FIO_SO
 sudo scripts/setup.sh
 
 ##Test -- check and FIO test
-./example/nvme/identity/identity
-sudo /usr/local/bin/fio latency-4k-rr-nvme.fio
+lsblk
+cat /proc/meminfo | grep -i huge
+mount | grep -i huge
+sudo scripts/setup.sh status
+sudo dpdk-17.02/usertools/dpdk-devbind.py --status
+
+sudo /home/ec2-user/spdk/examples/nvme/identify/identify > a.txt
+sudo /usr/local/bin/fio latency-4k-rr-spdk.fio >> a.txt
+sudo /usr/local/bin/fio latency-4k-rw-spdk.fio >> a.txt
+sudo /usr/local/bin/fio bw-1m-rr-spdk.fio >> a.txt
+sudo /usr/local/bin/fio bw-1m-rw-spdk.fio >> a.txt
+sudo /usr/local/bin/fio iops-4k-rr-spdk.fio >> a.txt
+sudo /usr/local/bin/fio iops-4k-rw-spdk.fio >> a.txts
+
+sudo scripts/setup.sh reset
+lsblk
+cat /proc/meminfo | grep -i huge
+mount | grep -i huge
